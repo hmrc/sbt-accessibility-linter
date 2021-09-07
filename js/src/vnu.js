c9e72675cc38d2b2f3593d9bbba7f21c7cddcc83
Oscar Duignan <100650+oscarduignan@users.noreply.github.com>
2021-09-07 10:42:31 +0100

    PLATUI-1339: bump scalatest library version
    Also removes VNU scripts, an embedded version of VNU is now provided
    by the scalatest-accessibility-linter library from 0.4.0

--- js/src/vnu.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-console */
-const runVnu = require('./vnu/vnu');
-
-runVnu(process.stdin).then((data) => {
-  console.log(data);
-}).catch((error) => {
-  console.error(error);
-  process.exit(1);
-});
